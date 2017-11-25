3ba25d177a4ad96ea59c4780e0da32f041253f04
Prasitha Thomas <prasitha.thomas@gmail.com>
2017-11-25 15:10:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,18 +19,18 @@ function destructivelyRemoveFirstDriver()
 
 function appendDriver(name)
 {
-  return (...drivers, name);
+  return [...drivers, name];
 }
 
 function prependDriver(name)
 {
-  return (name, ...drivers);
+  return [name, ...drivers];
 }
 function removeLastDriver()
 {
-  return drivers.slice(0, drivers.length - 1);
+  return [drivers.slice(0, drivers.length - 1)];
 }
 function removeFirstDriver()
 {
-return drivers.slice(1);
+return [drivers.slice(1)];
 }
